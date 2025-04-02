4509e68c58fdab27ce43180f9bc7df426d77d6fe
Donald Mxolisi <Donald>
2025-04-02 13:03:45 +0200

    new

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 require("dotenv").config();
 const app = require("./app");
 const sequelize = require("./config/db");
-const http = require('http');
-const { initWebSocketServer } = require('./websockets/wsServer'); 
+const https = require('https');
+const fs = require('fs');
+const { initWebSocketServer } = require('./websockets/wsServer');
 
-const PORT = process.env.PORT || 8080;
+// Define the paths to the SSL certificate files
+const CERT_PATH = '/etc/letsencrypt/live/play929.com/fullchain.pem';
+const KEY_PATH = '/etc/letsencrypt/live/play929.com/privkey.pem';
 
+// Set the port to 443 for HTTPS
+const PORT = 443;
 
-const server = http.createServer(app);
+const options = {
+    key: fs.readFileSync(KEY_PATH),
+    cert: fs.readFileSync(CERT_PATH),
+};
 
+const server = https.createServer(options, app);
 
 initWebSocketServer(server);
 
-
 sequelize.sync().then(() => {
     server.listen(PORT, () => {
-        console.log(`🚀 Server running on port ${PORT}`);
+        console.log(`🚀 Server running on https://play929.com:${PORT}`);
     });
 });
