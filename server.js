70c129c88463dae931b90ea878eb02e3aa664a47
Donald Mxolisi <donald.mxolisi@proton.me>
2025-04-20 14:37:26 +0200

    new

--- server.js
+++ server.js
@@ -3,33 +3,19 @@ const app = require("./app");
 const sequelize = require("./config/db");
 const fs = require("fs");
 const http = require("http");
-const https = require("https");
 const { initWebSocketServer } = require("./websockets/wsServer");
 
-const isProduction = process.env.NODE_ENV === "production";
-const PORT = isProduction ? 443 : 5000;
+const PORT =  5000;
 
-let server;
+// Create the server instance
+let server = http.createServer(app);
 
-if (isProduction) {
-    const CERT_PATH = '/etc/letsencrypt/live/play929.com/fullchain.pem';
-    const KEY_PATH = '/etc/letsencrypt/live/play929.com/privkey.pem';
-
-    const options = {
-        key: fs.readFileSync(KEY_PATH),
-        cert: fs.readFileSync(CERT_PATH),
-    };
-
-    server = https.createServer(options, app);
-} else {
-    server = http.createServer(app);
-}
-
+// Initialize the WebSocket server
 initWebSocketServer(server);
 
+// Sync database and start the server
 sequelize.sync().then(() => {
     server.listen(PORT, () => {
-        const protocol = isProduction ? "https" : "http";
-        console.log(`🚀 Server running on ${protocol}://localhost:${PORT}`);
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
 });
